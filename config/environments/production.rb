cb622449059a1b2070913620a076ea49c6247adc
Walther Højgaard Diechmann <walther@LilleBukkeBruse.local>
2019-05-25 14:25:49 +0200

    do most config via ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,10 +40,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  config.web_socket_server_url = "wss://sucker.alco.dk/cable"
-  config.action_cable.url = 'wss://sucker.alco.dk/cable'
-  config.action_cable.allowed_request_origins = [ 'https://sucker.alco.dk', /https:\/\/sucker.*/ ]
+  config.action_cable.mount_path = "#{ENV['ACTION_CABLE_MOUNT']}"
+  config.web_socket_server_url = "#{ENV['WS_SERVER_URL']}"
+  config.action_cable.url = "#{ENV['ACTION_CABLE_URL']}"
+  config.action_cable.allowed_request_origins = "#{ENV['ACTION_CABLE_ORIGIN']}"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount ActionCable.server, at: '/cable'
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
